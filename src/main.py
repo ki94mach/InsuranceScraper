75df76964a9c398140c533a3abc5f6334042ded5
ki94mach <kian.mohajeri@gmail.com>
2024-09-28 19:50:46 +0330

    Khadamat Excel ready for test

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from pkg.manager import  DataManager
 from pkg.tripleprice import TriplePrice 
 from pkg.scraper import WebScraper
 from pkg.processing import DataProcessing
+from pkg.khadamat_excel import data_store
 def main():
     tp_object = TriplePrice()
     # Download triple price and save it in a DataFrame
@@ -16,38 +17,48 @@ def main():
 
     # Insurance websites
     websites = ['Mosallah', 'Khadamat', 'Taamin']
-    choice = input('''
-          Please Select an Option:\n
-          1. All\n
-          2. Mossallah\n
-          3. Khadamat\n
-          4. Taamin\n
-          Enter your choice (1/2/3/4): 
-          ''')
-    selected_websites = []
-    if choice == '1':
-        selected_websites = websites
-    elif choice == '2':
-        selected_websites.append('Mosallah')
-    elif choice == '3':
-        selected_websites.append('Khadamat')
-    elif choice == '4':
-        selected_websites.append('Taamin')
-    else:
-        print("Invalid choice. Please enter 1, 2, 3, or 4.")
-        return
-    for website in selected_websites:
-        print(f"Running operations for {website}...")
-        scraper = WebScraper(website, generic_codes)
-        all_html, found_codes, not_found_codes = scraper.run_crawler()
-        processor = DataProcessing(website, generic_codes, all_html, found_codes, not_found_codes)
-        processor.parser()
-        processor.save_raw()
-        insurance_df = processor.clean_data()
-        # processor.code_count()
-        manager = DataManager(website, insurance_df, triple_price_df)
-        manager.storage()
-        insurance_update = manager.analysis()
-        manager.google_sheet_update()
+    khadamat_excel = data_store()
+    while True:
+        choice = input('''
+            Please Select an Option:\n
+            1. All\n
+            2. Mossallah\n
+            3. Khadamat\n
+            4. Taamin\n
+            5. Khadamat Excel\n
+            Enter your choice (1/2/3/4/5):\n
+            To exit enter Q\n
+            ''')
+        selected_websites = []
+        if choice == "Q":
+            break
+        elif choice == '1':
+            khadamat_excel.run()
+            selected_websites = websites
+        elif choice == '2':
+            selected_websites.append('Mosallah')
+        elif choice == '3':
+            khadamat_excel.run()
+            selected_websites.append('Khadamat')
+        elif choice == '4':
+            selected_websites.append('Taamin')
+        elif choice == '5':
+            khadamat_excel.run()
+        else:
+            print("Invalid choice. Please enter 1, 2, 3, or 4.")
+            continue
+        for website in selected_websites:
+            print(f"Running operations for {website}...")
+            scraper = WebScraper(website, generic_codes)
+            all_html, found_codes, not_found_codes = scraper.run_crawler()
+            processor = DataProcessing(website, generic_codes, all_html, found_codes, not_found_codes)
+            processor.parser()
+            processor.save_raw()
+            insurance_df = processor.clean_data()
+            # processor.code_count()
+            manager = DataManager(website, insurance_df, triple_price_df)
+            manager.storage()
+            insurance_update = manager.analysis()
+            manager.google_sheet_update()
 if __name__ == "__main__":
     main()
--- src/pkg/khadamat_excel.py
+++ src/pkg/khadamat_excel.py
@@ -4,145 +4,126 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from io import BytesIO
 import os
+import jdatetime
+class data_store:
+    def __init__(self):
+        self.current_date = jdatetime.datetime.now().strftime('%Y/%m/%d')
 
-# Define the URL
-url = 'https://mdp.ihio.gov.ir/'
+    def _downloader(self):
 
-# Define headers (excluding 'Content-Type' and 'Cookie')
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br, zstd',
-    'Origin': 'https://mdp.ihio.gov.ir',
-    'Connection': 'keep-alive',
-    'Referer': 'https://mdp.ihio.gov.ir/',
-    'Upgrade-Insecure-Requests': '1',
-    'Sec-Fetch-Dest': 'iframe',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-User': '?1',
-    'Priority': 'u=4',
-}
+        url = 'https://mdp.ihio.gov.ir/'
 
-# Create a session
-session = requests.Session()
-session.headers.update(headers)
-
-# Initial GET request to get dynamic fields and establish a session
-response_get = session.get(url)
-soup = BeautifulSoup(response_get.text, 'html.parser')
-
-# Extract dynamic fields
-viewstate_generator = soup.find('input', {'name': '__VIEWSTATEGENERATOR'})
-if viewstate_generator:
-    viewstate_generator_value = viewstate_generator['value']
-else:
-    viewstate_generator_value = ''
-
-# Update data with dynamic fields
-data = {
-    'cmbServiceType': 'داروخانه',
-    '_cmbServiceType_state': '[{"value":"842","text":"\\u062f\\u0627\\u0631\\u0648\\u062e\\u0627\\u0646\\u0647","index":0}]',
-    'txtSrchSrvCode': '',
-    'txtSrchSrvName': '',
-    'txtSrchChgDate': '',
-    'txtSrchChgDate2': '',
-    'txtSerSize': '',
-    'txtSrchFeranshiz': '',
-    'cmbSrchSrvChgStatus': 'آخرين تغييرات',
-    '_cmbSrchSrvChgStatus_state': '',
-    'ComboBox1': '10',
-    '_ComboBox1_state': '[{"value":"10","text":"10","index":0}]',
-    '__VIEWSTATEGENERATOR': viewstate_generator_value,
-    '__EVENTTARGET': 'ResourceManager1',
-    '__EVENTARGUMENT': 'btnExcelExport|event|Click',
-    '__ExtNetDirectEventMarker': 'delta=true',
-}
-
-# Construct the values dictionary for 'submitDirectEventConfig'
-values_dict = {
-    "cmbServiceType": "842",
-    "_cmbServiceType_state": "[{\"value\":\"842\",\"text\":\"\\u062f\\u0627\\u0631\\u0648\\u062e\\u0627\\u0646\\u0647\",\"index\":0}]",
-    "txtSrchSrvCode": "",
-    "txtSrchSrvName": "",
-    "txtSrchChgDate": "",
-    "txtSrchChgDate2": "",
-    "txtSerSize": "",
-    "txtSrchFeranshiz": "",
-    "cmbSrchSrvChgStatus": "",
-    "_cmbSrchSrvChgStatus_state": "",
-    "cmbDeviceGroup": "",
-    "_cmbDeviceGroup_state": "",
-    "txtSerchUniversal": "",
-    "cmbSrchFehrest": "",
-    "_cmbSrchFehrest_state": "",
-    "cmbSrchExpert": "",
-    "_cmbSrchExpert_state": "",
-    "cmbSrchRes": "",
-    "_cmbSrchRes_state": "",
-    "cmbSrchSpc": "",
-    "_cmbSrchSpc_state": "",
-    "cmbBimeProdType": "",
-    "_cmbBimeProdType_state": "",
-}
-
-# Construct 'submitDirectEventConfig'
-submit_direct_event_config = {
-    "config": {
-        "extraParams": {
-            "values": json.dumps(values_dict, ensure_ascii=False)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Origin': 'https://mdp.ihio.gov.ir',
+            'Connection': 'keep-alive',
+            'Referer': 'https://mdp.ihio.gov.ir/',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'iframe',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-User': '?1',
+            'Priority': 'u=4',
         }
-    }
-}
 
-submit_direct_event_config_json = json.dumps(submit_direct_event_config, ensure_ascii=False)
-data['submitDirectEventConfig'] = submit_direct_event_config_json
 
-# Files (for empty file upload field)
-files = {
-    'ucServiceDoc_fileDocument': ('', '', 'application/octet-stream'),
-}
+        session = requests.Session()
+        session.headers.update(headers)
 
-# Send the POST request
-response = session.post(url, data=data, files=files)
+        response_get = session.get(url)
+        soup = BeautifulSoup(response_get.text, 'html.parser')
 
-# Handle the response
-if response.status_code == 200:
-    content_type = response.headers.get('Content-Type', '').lower()
-    if 'application/vnd.ms-excel' in content_type.lower() or 'application/x-msexcel' in content_type.lower():
-        # Read the Excel file from response content into a DataFrame
-        df_current = pd.read_excel(BytesIO(response.content))
-        
-        # Load the existing record DataFrame if it exists
-        record_file = 'data/total_khadamat.xlsx'
-        if os.path.exists(record_file):
-            df_record = pd.read_excel(record_file)
+        viewstate_generator = soup.find('input', {'name': '__VIEWSTATEGENERATOR'})
+        if viewstate_generator:
+            viewstate_generator_value = viewstate_generator['value']
         else:
-            df_record = pd.DataFrame()
-        
-       # Append current data to the record data
-        df_combined = pd.concat([df_record, df_current], ignore_index=True)
-        
-        # Define the column to exclude from duplication check
-        variable_column = 'VariableColumn'  # Replace with your actual column name to exclude
-        
-        # Check if the variable_column exists
-        if variable_column not in df_combined.columns:
-            print(f"The column '{variable_column}' was not found in the data.")
+            viewstate_generator_value = ''
+        data = {
+            'cmbServiceType': 'داروخانه',
+            '_cmbServiceType_state': '[{"value":"842","text":"\\u062f\\u0627\\u0631\\u0648\\u062e\\u0627\\u0646\\u0647","index":0}]',
+            'txtSrchSrvCode': '',
+            'txtSrchSrvName': '',
+            'txtSrchChgDate': '',
+            'txtSrchChgDate2': '',
+            'txtSerSize': '',
+            'txtSrchFeranshiz': '',
+            'cmbSrchSrvChgStatus': 'آخرين تغييرات',
+            '_cmbSrchSrvChgStatus_state': '',
+            'ComboBox1': '10',
+            '_ComboBox1_state': '[{"value":"10","text":"10","index":0}]',
+            '__VIEWSTATEGENERATOR': viewstate_generator_value,
+            '__EVENTTARGET': 'ResourceManager1',
+            '__EVENTARGUMENT': 'btnExcelExport|event|Click',
+            '__ExtNetDirectEventMarker': 'delta=true',
+        }
+
+        values_dict = {
+            "cmbServiceType": "842",
+            "_cmbServiceType_state": "[{\"value\":\"842\",\"text\":\"\\u062f\\u0627\\u0631\\u0648\\u062e\\u0627\\u0646\\u0647\",\"index\":0}]",
+            "txtSrchSrvCode": "",
+            "txtSrchSrvName": "",
+            "txtSrchChgDate": "",
+            "txtSrchChgDate2": "",
+            "txtSerSize": "",
+            "txtSrchFeranshiz": "",
+            "cmbSrchSrvChgStatus": "",
+            "_cmbSrchSrvChgStatus_state": "",
+            "cmbDeviceGroup": "",
+            "_cmbDeviceGroup_state": "",
+            "txtSerchUniversal": "",
+            "cmbSrchFehrest": "",
+            "_cmbSrchFehrest_state": "",
+            "cmbSrchExpert": "",
+            "_cmbSrchExpert_state": "",
+            "cmbSrchRes": "",
+            "_cmbSrchRes_state": "",
+            "cmbSrchSpc": "",
+            "_cmbSrchSpc_state": "",
+            "cmbBimeProdType": "",
+            "_cmbBimeProdType_state": "",
+        }
+
+        submit_direct_event_config = {
+            "config": {
+                "extraParams": {
+                    "values": json.dumps(values_dict, ensure_ascii=False)
+                }
+            }
+        }
+
+        submit_direct_event_config_json = json.dumps(submit_direct_event_config, ensure_ascii=False)
+        data['submitDirectEventConfig'] = submit_direct_event_config_json
+
+        files = {
+            'ucServiceDoc_fileDocument': ('', '', 'application/octet-stream'),
+        }
+
+        self.response = session.post(url, data=data, files=files)
+
+    def _process(self):
+        if self.response.status_code == 200:
+            content_type = self.response.headers.get('Content-Type', '').lower()
+            if 'application/vnd.ms-excel' in content_type.lower() or 'application/x-msexcel' in content_type.lower():
+                self.df_current = pd.read_excel(BytesIO(self.response.content))
+                self.record_file = 'data/Khadamat_excel.xlsx'
+                if os.path.exists(self.record_file):
+                    self.df_record = pd.read_excel(self.record_file)
+                else:
+                    self.df_record = pd.DataFrame()
+                self.df_current['recorded_date'] = self.current_date
+                df_combined = pd.concat([self.df_record, self.df_current], ignore_index=True)
+                variable_column = ['رديف', 'recorded_date'] 
+                cols_to_check = [col for col in df_combined.columns if col not in variable_column]
+                df_deduplicated = df_combined.drop_duplicates(subset=cols_to_check, keep='first')
+                df_deduplicated.to_excel(self.record_file, index=False)
+                print('Khadamat excel file updated successfully.')
+            else:
+                print('Response is not an Excel file. Content-Type:', content_type)
         else:
-            # Remove duplicates considering all columns except variable_column
-            cols_to_check = [col for col in df_combined.columns if col != variable_column]
-            df_deduplicated = df_combined.drop_duplicates(subset=cols_to_check, keep='last')
-            
-            # Save the deduplicated DataFrame to the record file
-            df_deduplicated.to_excel(record_file, index=False)
-            print('Record file updated successfully.')
-            
-            # Optionally, report the number of duplicates removed
-            num_duplicates_removed = len(df_combined) - len(df_deduplicated)
-            print(f"Duplicates removed: {num_duplicates_removed}")
-    else:
-        print('Response is not an Excel file. Content-Type:', content_type)
-else:
-    print(f'Failed to download file. Status code: {response.status_code}')
+            print(f'Failed to download file. Status code: {self.response.status_code}')
+    def run(self):
+        self._downloader()
+        self._process()
